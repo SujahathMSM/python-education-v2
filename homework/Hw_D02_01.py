3225e366aa4ca76342f47f1477134d0c41e129a0
SujahathMSM <78421570+SujahathMSM@users.noreply.github.com>
2021-02-02 20:28:24 +0530

    Add files via upload

---
+++ homework/Hw_D02_01.py
@@ -0,0 +1,5 @@
+My_list=[1,4,9,16,25,36,10,20,30,40,50,60]
+New_List=[]
+first_Half=My_list[0:6]
+Second_Half=My_list[6:]
+print(Second_Half+first_Half)
---
+++ homework/Hw_D02_02.py
@@ -0,0 +1,4 @@
+n=int(input('Enter a number: '))
+l=[i for i in range(n+1) if i%2==0]
+for j in l:
+    print(j)
